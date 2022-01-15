7d9056ffd3dbc8d25bd59f111c08a127a0a70438
emilywang98 <68518246+Emilywang98@users.noreply.github.com>
2022-01-14 20:16:50 -0700

    commit changes

---
+++ src/FrontPage.jsx
@@ -0,0 +1,11 @@
+const FrontPage = () => {
+    return ( 
+        <div>
+            <h1>Hello Cloud!</h1>
+        </div>
+    );
+}
+    <div>
+        <h1>Hello Cloud!</h1>
+    </div>
+export default FrontPage;
